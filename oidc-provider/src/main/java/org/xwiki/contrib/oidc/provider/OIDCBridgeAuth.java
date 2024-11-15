fbe77286240b810991182b23d9ad01084000d4f8
Thomas Mortagne <thomas.mortagne@gmail.com>
2024-11-15 14:22:12 +0100

    OIDC-201: It's possible to end up with a broken token authenticator fallback when uninstalled an extension

--- oidc-provider/src/main/java/org/xwiki/contrib/oidc/provider/OIDCBridgeAuth.java
+++ oidc-provider/src/main/java/org/xwiki/contrib/oidc/provider/OIDCBridgeAuth.java
@@ -45,57 +45,64 @@ public class OIDCBridgeAuth implements XWikiAuthService
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(OIDCBridgeAuth.class);
 
-    private XWikiAuthService authService;
+    @SuppressWarnings("java:S3077")
+    private volatile XWikiAuthService authService;
 
     private OIDCProviderAuthenticator oidcAuthenticator = Utils.getComponent(OIDCProviderAuthenticator.class);
 
     private OIDCProviderConfiguration configuration = Utils.getComponent(OIDCProviderConfiguration.class);
 
     /**
-     * Initialize the wrapped authenticator.
+     * @return the configured "real" authenticator to fallback to when there is no token
+     * @throws XWikiException when failing to get the authenticator
      */
-    public OIDCBridgeAuth()
+    private XWikiAuthService getAuthService() throws XWikiException
     {
-        String auth = this.configuration.getAuthenticator();
-
-        createAuthService(auth);
-    }
-
-    private void createAuthService(String auth)
-    {
-        if (StringUtils.isNotEmpty(auth)) {
-            LOGGER.debug("Using custom AuthClass [{}].", auth);
-
-            try {
-                // Get the current ClassLoader
-                @SuppressWarnings("deprecation")
-                ClassLoaderManager clManager = Utils.getComponent(ClassLoaderManager.class);
-                ClassLoader classloader = null;
-                if (clManager != null) {
-                    classloader = clManager.getURLClassLoader("wiki:xwiki", false);
+        if (this.authService == null) {
+            synchronized (this) {
+                if (this.authService == null) {
+                    try {
+                        this.authService = loadAuthService();
+                    } catch (Exception e) {
+                        throw new XWikiException("Failed to create the configured authenticator", e);
+                    }
                 }
-
-                // Get the class
-                if (classloader != null) {
-                    this.authService = (XWikiAuthService) Class.forName(auth, true, classloader).newInstance();
-                } else {
-                    this.authService = (XWikiAuthService) Class.forName(auth).newInstance();
-                }
-
-                LOGGER.debug("Initialized AuthService using Reflection.");
-
-                return;
-            } catch (Exception e) {
-                LOGGER.warn("Failed to initialize AuthService " + auth
-                    + " using Reflection, trying default implementations using 'new'.", e);
             }
         }
 
-        this.authService = new XWikiAuthServiceImpl();
+        return this.authService;
+    }
 
-        if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug("Initialized AuthService [{}] using 'new'.", this.authService.getClass().getName());
+    private XWikiAuthService loadAuthService()
+        throws InstantiationException, IllegalAccessException, ClassNotFoundException
+    {
+        String auth = this.configuration.getAuthenticator();
+
+        if (StringUtils.isNotEmpty(auth)) {
+            LOGGER.debug("Using custom AuthClass [{}].", auth);
+
+            // Get the current ClassLoader
+            @SuppressWarnings("deprecation")
+            ClassLoaderManager clManager = Utils.getComponent(ClassLoaderManager.class);
+            ClassLoader classloader = null;
+            if (clManager != null) {
+                classloader = clManager.getURLClassLoader("wiki:xwiki", false);
+            }
+
+            Class<?> serviceClass;
+            // Get the class
+            if (classloader != null) {
+                serviceClass = Class.forName(auth, true, classloader);
+            } else {
+                serviceClass = Class.forName(auth);
+            }
+
+            return (XWikiAuthService) serviceClass.newInstance();
         }
+
+        LOGGER.debug("Not custom auth class indicated, use the standard one");
+
+        return new XWikiAuthServiceImpl();
     }
 
     @Override
@@ -113,25 +120,25 @@ public class OIDCBridgeAuth implements XWikiAuthService
             LOGGER.debug("Failed to get OIDC user from HTTP authorization [" + authorization + "]", e);
         }
 
-        return this.authService.checkAuth(context);
+        return getAuthService().checkAuth(context);
     }
 
     @Override
     public void showLogin(XWikiContext context) throws XWikiException
     {
-        this.authService.showLogin(context);
+        getAuthService().showLogin(context);
     }
 
     @Override
     public XWikiUser checkAuth(String username, String password, String rememberme, XWikiContext context)
         throws XWikiException
     {
-        return this.authService.checkAuth(username, password, rememberme, context);
+        return getAuthService().checkAuth(username, password, rememberme, context);
     }
 
     @Override
     public Principal authenticate(String username, String password, XWikiContext context) throws XWikiException
     {
-        return this.authService.authenticate(username, password, context);
+        return getAuthService().authenticate(username, password, context);
     }
 }
